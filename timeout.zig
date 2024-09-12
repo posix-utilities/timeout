bd31d749d63d631a1202af198c6d17d69cff9534
AJ ONeal <aj@therootcompany.com>
2024-09-12 04:49:19 -0600

    ref(zig): use threads instead of sleep + polling, fix pgid

--- timeout.zig
+++ timeout.zig
@@ -1,8 +1,28 @@
+// zig v0.14.0-dev.1511+54b668f8a (2024-09-12)
+// (because 'std.process.Child.pgid' was added after v0.13.0)
+
 const std = @import("std");
+const builtin = @import("builtin");
 const fs = std.fs;
 const Signal = u8;
 const SIG = std.c.SIG;
 const Allocator = std.mem.Allocator;
+const AtomicBool = std.atomic.Value(bool);
+const native_os = builtin.os.tag;
+
+const timeout_context = struct {
+    child: *std.process.Child,
+    timeout_reached: *AtomicBool,
+    child_exited: *AtomicBool,
+    term_delay: u64,
+    kill_delay: ?u64,
+    pid: std.process.Child.Id,
+    signal: Signal,
+};
+var child_term: ?std.process.Child.Term = null;
+
+const second_ns = 1_000_000_000; // 1s in nanos
+// const ms = 1_000_000; // 1ms in nanos
 
 fn parse_signal(signal_name: []const u8) Signal {
     if (std.mem.eql(u8, signal_name, "term")) {
@@ -21,8 +41,9 @@ fn parse_signal(signal_name: []const u8) Signal {
 fn parse_duration(duration: []const u8) u64 {
     const value: []const u8 = duration[0 .. duration.len - 1];
     const suffix: u8 = duration[duration.len - 1];
-    const parsed_value: u64 = std.fmt.parseInt(u64, value, 10) catch {
-        std.debug.print("Failed to parse duration.\n", .{});
+    const parsed_value: u64 = std.fmt.parseInt(u64, value, 10) catch |e| {
+        std.debug.print("could not parse duration '{any}': {any}\n", .{ value, e });
+        std.process.exit(125);
         return 0;
     };
 
@@ -33,6 +54,82 @@ fn parse_duration(duration: []const u8) u64 {
         'd' => parsed_value * 86400, // days
         else => parsed_value, // default to seconds if no suffix
     };
+}
+
+fn wait_for_child(ctx: *const timeout_context) void {
+    child_term = ctx.child.wait() catch |e| {
+        ctx.child_exited.store(true, .seq_cst);
+        switch (e) {
+            error.FileNotFound => {
+                std.process.exit(127);
+            },
+            error.AccessDenied => {
+                std.process.exit(126);
+            },
+            else => {
+                std.debug.print("error waiting on child to exit: {any}\n", .{e});
+                std.process.exit(125);
+            },
+        }
+        return;
+    };
+    ctx.child_exited.store(true, .seq_cst);
+
+    if (ctx.timeout_reached.load(.seq_cst)) {
+        return;
+    }
+
+    if (child_term) |term| {
+        switch (term) {
+            std.process.Child.Term.Exited => |status| {
+                std.process.exit(status);
+            },
+            else => {
+                std.debug.print("unsupported return type: {any}\n", .{term});
+                std.process.exit(125); // unknown error
+            },
+        }
+    }
+}
+
+fn wait_for_timeout(ctx: *const timeout_context) void {
+    std.time.sleep(ctx.term_delay);
+    if (ctx.child_exited.load(.seq_cst)) {
+        return;
+    }
+
+    ctx.timeout_reached.store(true, .seq_cst);
+    std.posix.kill(ctx.pid, ctx.signal) catch |e| {
+        switch (e) {
+            std.posix.KillError.ProcessNotFound => {
+                return;
+            },
+            else => {
+                std.debug.print("error signaling child process: {any}\n", .{e});
+                std.process.exit(125);
+            },
+        }
+    };
+
+    if (ctx.kill_delay) |kill_delay| {
+        std.time.sleep(kill_delay);
+        if (ctx.child_exited.load(.seq_cst)) {
+            return;
+        }
+
+        // ctx.kill_reached.store(true, .seq_cst);
+        std.posix.kill(ctx.pid, SIG.KILL) catch |e| {
+            switch (e) {
+                std.posix.KillError.ProcessNotFound => {
+                    return;
+                },
+                else => {
+                    std.debug.print("error killing child process: {any}\n", .{e});
+                    std.process.exit(125);
+                },
+            }
+        };
+    }
 }
 
 pub fn main() !void {
@@ -46,7 +143,8 @@ pub fn main() !void {
     }
 
     if (arg_count < 3) {
-        std.debug.print("Usage: timeout [-fp] [-k time] [-s signal_name] duration utility [argument...]\n", .{});
+        std.debug.print("USAGE\n\ttimeout [-f] [-p] [-k duration] [-s signal] <duration> <command> [arguments...]\n\n", .{});
+        std.debug.print("EXAMPLE\n\ttimeout -p -k 3s -s TERM 1.5m <command> [arguments...]\n\n", .{});
         return;
     }
 
@@ -65,7 +163,6 @@ pub fn main() !void {
         switch (arg[0]) {
             '-' => {
                 switch (arg[1]) {
-                    '-' => break,
                     'f' => signal_child_only = true,
                     'p' => preserve_status = true,
                     's' => {
@@ -77,25 +174,26 @@ pub fn main() !void {
                         kill_after = parse_duration(current_arg.?);
                     },
                     else => {
-                        // ignore
+                        std.debug.print("unknown option: {s}\n", .{arg});
+                        std.process.exit(125);
                     },
                 }
             },
             else => {
                 current_arg = arg;
-                std.debug.print("breaking_arg dur: {?s}\n", .{current_arg});
                 break;
             },
         }
     }
+    const kill_delay = if (kill_after) |kill_after_s| kill_after_s * second_ns else null;
 
     const duration = parse_duration(current_arg.?);
-    std.debug.print("current_arg dur: {?s} {any}\n", .{ current_arg, duration });
+    const term_delay = duration * second_ns;
 
     current_arg = arg_iter2.next();
-    const utility = current_arg.?;
+    const command = current_arg.?;
     var mut_args = std.ArrayList([]const u8).init(allocator);
-    try mut_args.append(utility);
+    try mut_args.append(command);
     while (arg_iter2.next()) |arg| {
         try mut_args.append(arg);
     }
@@ -106,110 +204,54 @@ pub fn main() !void {
         allocator,
     );
     child.cwd_dir = fs.cwd();
+    if (native_os != .windows and native_os != .wasi) {
+        if (!signal_child_only) {
+            child.pgid = 0; // not available in v0.13.0
+        }
+    }
 
-    // const s = 1_000_000_000; // 1s in nanos
-    const ms = 1_000_000; // 1ms in nanos
-    //
-    var timed_out = false;
-    const start_time = std.time.milliTimestamp();
-    const check_delay = 100 * ms; // TODO thread and join instead of poll?
-    child.spawn() catch {
-        std.debug.print("Failed to start utility: {s}\n", .{utility});
+    child.spawn() catch |e| {
+        std.debug.print("could not exec command '{s}': {any}\n", .{ command, e });
+        std.process.exit(126);
         return;
     };
 
-    var pid = -child.id;
-    if (signal_child_only) {
-        pid = child.id;
-    }
+    const pid = if (native_os != .windows and native_os != .wasi) child.id else if (signal_child_only) child.id else -child.id;
 
-    const wait_timeout = duration * 1000;
-    while (true) {
-        std.time.sleep(check_delay);
+    var timeout_reached = AtomicBool.init(false);
+    var child_exited = AtomicBool.init(false);
 
-        const status = std.posix.kill(child.id, 0) catch |e| {
-            std.debug.print("timeout check error: {any} {any}\n", .{ child.id, e });
-            switch (e) {
-                std.posix.KillError.ProcessNotFound => {
-                    break;
-                },
-                else => {},
-            }
-        };
-        std.debug.print("timeout check result: {any} {any}\n", .{ child.id, status });
-
-        const now = std.time.milliTimestamp();
-        const delta = now - start_time;
-        if (delta < wait_timeout) {
-            continue;
-        }
-
-        timed_out = true;
-        std.posix.kill(pid, signal) catch |e| {
-            switch (e) {
-                std.posix.KillError.ProcessNotFound => {
-                    break;
-                },
-                else => {
-                    std.debug.print("error killing child process: {any}\n", .{e});
-                    std.process.exit(125);
-                },
-            }
-        };
-    }
-
-    if (kill_after) |kill_after_s| {
-        const wait_kill = kill_after_s * 1000;
-        const start_kill = std.time.milliTimestamp();
-        while (true) {
-            std.time.sleep(check_delay);
-
-            const status = std.posix.kill(child.id, 0) catch |e| {
-                std.debug.print("kill check error: {any}\n", .{e});
-                switch (e) {
-                    std.posix.KillError.ProcessNotFound => {
-                        break;
-                    },
-                    else => {},
-                }
-            };
-            std.debug.print("kill check result: {any}\n", .{status});
-
-            const now = std.time.milliTimestamp();
-            const delta = now - start_kill;
-            if (delta >= wait_kill) {
-                std.posix.kill(pid, SIG.KILL) catch |e| {
-                    switch (e) {
-                        std.posix.KillError.ProcessNotFound => {
-                            break;
-                        },
-                        else => {},
-                    }
-                };
-                break;
-            }
-        }
-    }
-
-    const exit_status = child.wait() catch |e| {
-        std.debug.print("error while waiting for child process: {any}\n", .{e});
-        std.process.exit(125);
+    const cfg = std.Thread.SpawnConfig{
+        .allocator = allocator,
     };
+    const ctx = &timeout_context{
+        .child = &child,
+        .child_exited = &child_exited,
+        .timeout_reached = &timeout_reached,
+        .term_delay = term_delay,
+        .kill_delay = kill_delay,
+        .pid = pid,
+        .signal = signal,
+    };
+    var child_thread = try std.Thread.spawn(cfg, wait_for_child, .{ctx});
+    _ = try std.Thread.spawn(cfg, wait_for_timeout, .{ctx});
 
+    child_thread.join();
     if (!preserve_status) {
-        const code: u8 = if (timed_out) 124 else 0;
-        std.process.exit(code);
+        std.process.exit(124);
         return;
     }
 
-    switch (exit_status) {
-        std.process.Child.Term.Exited => |status| {
-            std.process.exit(status);
-        },
-        else => {
-            std.process.exit(125);
-        },
+    if (child_term) |e| {
+        switch (e) {
+            std.process.Child.Term.Exited => |status| {
+                std.process.exit(status);
+            },
+            else => {
+                std.process.exit(125);
+            },
+        }
     }
 
-    std.debug.print("Timeout finished.\n", .{});
+    std.process.exit(125);
 }
